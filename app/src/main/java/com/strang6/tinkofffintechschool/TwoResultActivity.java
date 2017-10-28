368383ed7656955852182657c1de013c67ed24f8
Strang6 <dariagorod@gmail.com>
2017-10-28 15:39:30 +0300

    onRestart && InterruptedException in sleep

--- app/src/main/java/com/strang6/tinkofffintechschool/TwoResultActivity.java
+++ app/src/main/java/com/strang6/tinkofffintechschool/TwoResultActivity.java
@@ -52,39 +52,48 @@ public class TwoResultActivity extends AppCompatActivity {
         Log.d(TAG, "onStop");
     }
 
+    @Override
+    protected void onRestart() {
+        if (author == null || dob == null) {
+            loadAuthorInfoThread.start();
+        }
+        super.onRestart();
+        Log.d(TAG, "onRestart");
+    }
+
     private Thread loadAuthorInfoThread = new Thread() {
         private String [] authorInfo = {"Александр Сергеевич Пушкин",
                 "(26 мая [6 июня] 1799, Москва — 29 января [10 февраля] 1837, Санкт-Петербург)"};
         @Override
         public void run() {
             Log.d(TAG, "run");
-            if (interrupted())
+            if (this.isInterrupted())
                 return;
             Message message = handler.obtainMessage();
             message.what = AUTHOR_MESSAGE;
             Bundle bundle = new Bundle();
             bundle.putString(AUTHOR_KEY,authorInfo[0]);
-            if (interrupted())
+            if (this.isInterrupted())
                 return;
             message.setData(bundle);
-            if (interrupted())
+            if (this.isInterrupted())
                 return;
             handler.sendMessage(message);
-            if (interrupted())
+            if (this.isInterrupted())
                 return;
             try {
                 Thread.sleep(5000);
             } catch (InterruptedException e) {
-                e.printStackTrace();
+                Thread.currentThread().interrupt();
             }
-            if (interrupted())
+            if (this.isInterrupted())
                 return;
             message = handler.obtainMessage();
             message.what = DOB_MESSAGE;
             bundle = new Bundle();
             bundle.putString(DOB_KEY,authorInfo[1]);
             message.setData(bundle);
-            if (interrupted())
+            if (this.isInterrupted())
                 return;
             handler.sendMessage(message);
         }
@@ -97,13 +106,13 @@ public class TwoResultActivity extends AppCompatActivity {
             Bundle bundle = msg.getData();
             switch (msg.what) {
                 case AUTHOR_MESSAGE:
-                    Log.d(TAG, "AUTHOR_MESSAGE");
                     author = bundle.getString(AUTHOR_KEY);
+                    Log.d(TAG, "AUTHOR_MESSAGE " + author);
                     authorTextView.setText(author);
                     break;
                 case DOB_MESSAGE:
-                    Log.d(TAG, "DOB_MESSAGE");
                     dob = bundle.getString(DOB_KEY);
+                    Log.d(TAG, "DOB_MESSAGE " + dob);
                     dobTextView.setText(dob);
                     break;
             }
